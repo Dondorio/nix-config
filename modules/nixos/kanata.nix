ecac5300e73d3e5cc89244b65ae2d6ecc9f6fd04
Donda <dondadecahedron@gmail.com>
2025-10-27 19:33:05 +0100

    make kanata use colemak-dh as the src

--- modules/nixos/kanata.nix
+++ modules/nixos/kanata.nix
@@ -63,14 +63,21 @@
             spc (tap-hold $tap-time $hold-time spc (layer-while-held space-fn))
             )
 
-            ;;(deflayer base
-            ;; @caps @a  @s  @d  @f  @j  @k  @l  @;
-            ;;)
+            ;; Physical qwerty
+            ;; (defsrc
+            ;; grv  1    2    3    4    5    6    7    8    9    0    -    =    bspc
+            ;; tab  q    w    e    r    t    y    u    i    o    p    [    ]    \
+            ;; caps a    s    d    f    g    h    j    k    l    ;    '    ret
+            ;; lsft z    x    c    v    b    n    m    ,    .    /    rsft
+            ;; lctl lmet lalt           spc            ralt rmet rctl
+            ;; )
+
+            ;; Physical colemak-dh
             (defsrc
             grv  1    2    3    4    5    6    7    8    9    0    -    =    bspc
-            tab  q    w    e    r    t    y    u    i    o    p    [    ]    \
-            caps a    s    d    f    g    h    j    k    l    ;    '    ret
-            lsft z    x    c    v    b    n    m    ,    .    /    rsft
+            tab  q    w    f    p    b    j    l    u    y    ;    [    ]    \
+            caps a    r    s    t    g    m    n    e    i    o    '    ret
+            lsft z    x    c    d    v    k    h    ,    .    /    rsft
             lctl lmet lalt           spc            ralt rmet rctl
             )
 
